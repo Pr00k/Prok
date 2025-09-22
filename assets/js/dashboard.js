104f382f3f3bff400c2f1594b8550b4b69ac2565
Prooki <75479500+Prooki@users.noreply.github.com>
2025-09-22 10:57:41 +0300

    Update dashboard.js

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,28 +1,9 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+// منع أي شخص يدخل لوحة التحكم بدون تسجيل دخول
+if (sessionStorage.getItem("adminLoggedIn") !== "true") {
+  window.location.href = "index.html";
+}
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDfy48kwyj8iAu20hx3uJtzcbRGcfcKKn0",
-  authDomain: "prok-58f05.firebaseapp.com",
-  projectId: "prok-58f05",
-  storageBucket: "prok-58f05.appspot.com",
-  messagingSenderId: "978563434886",
-  appId: "1:978563434886:web:d16c70551240a05c81c407",
-  measurementId: "G-PWTGTT2VJT"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-// عند الضغط على زر الحفظ
-document.getElementById("saveBtn").addEventListener("click", async () => {
-  const title = document.getElementById("siteTitle").value;
-  const bg = document.getElementById("siteBg").value;
-
-  await setDoc(doc(db, "siteContent", "homepage"), {
-    title: title,
-    background: bg
-  });
-
-  alert("✅ تم حفظ التغييرات");
+document.getElementById("logoutBtn").addEventListener("click", () => {
+  sessionStorage.removeItem("adminLoggedIn");
+  window.location.href = "index.html";
 });
