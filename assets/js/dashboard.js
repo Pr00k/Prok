e8741dde1301895495318bee2e628d17085bfe6e
Pr00k <75479500+Pr00k@users.noreply.github.com>
2025-10-04 03:39:14 +0300

    Delete assets directory

--- assets/js/dashboard.js
+++
@@ -1,9 +0,0 @@
-// منع أي شخص يدخل لوحة التحكم بدون تسجيل دخول
-if (sessionStorage.getItem("adminLoggedIn") !== "true") {
-  window.location.href = "index.html";
-}
-
-document.getElementById("logoutBtn").addEventListener("click", () => {
-  sessionStorage.removeItem("adminLoggedIn");
-  window.location.href = "index.html";
-});
