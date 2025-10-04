fd39e376f9c72c0779b02919ebd3010a0de96e18
Pr00k <75479500+Pr00k@users.noreply.github.com>
2025-10-04 21:50:35 +0300

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,18 +1,15 @@
-// assets/js/firebase-config.js
-
-// إعدادات Firebase الخاصة بمشروعك الحقيقي
+// Firebase Config (نهائي ومعدّل)
 const firebaseConfig = {
   apiKey: "AIzaSyDfy48kwyj8iAu20hx3uJtzcbRGcfcKKn0",
   authDomain: "prok-58f05.firebaseapp.com",
   projectId: "prok-58f05",
-  storageBucket: "prok-58f05.appspot.com", // ← لاحظ التعديل هنا
+  storageBucket: "prok-58f05.appspot.com",
   messagingSenderId: "978563434886",
   appId: "1:978563434886:web:d16c70551240a05c81c407",
   measurementId: "G-PWTGTT2VJT"
 };
 
-// تشغيل Firebase (النسخة الحديثة)
 if (window.firebase && !firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
-  console.log("🔥 Firebase تم تشغيله بنجاح!");
+  console.log("🔥 Firebase متصل بنجاح!");
 }
