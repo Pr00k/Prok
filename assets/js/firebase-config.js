3d99db3e519193a9225de93bd50e59c95bc9fb51
Pr00k <75479500+Pr00k@users.noreply.github.com>
2025-10-04 15:59:10 +0300

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,10 +1,13 @@
-// Paste your Firebase config object here (replace values)
-window.firebaseConfig = {
-  apiKey: "AIza...",
-  authDomain: "yourapp.firebaseapp.com",
-  projectId: "yourapp",
-  storageBucket: "yourapp.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:2345:abcdef",
-  measurementId: ""
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDfy48kwyj8iAu20hx3uJtzcbRGcfcKKn0",
+  authDomain: "prok-58f05.firebaseapp.com",
+  projectId: "prok-58f05",
+  storageBucket: "prok-58f05.appspot.com",
+  messagingSenderId: "978563434886",
+  appId: "1:978563434886:web:d16c70551240a05c81c407",
+  measurementId: "G-PWTGTT2VJT"
 };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
