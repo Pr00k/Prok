cddbb8d28024cdc2fc7b7ed560d88e9fbd39005b
Pr00k <75479500+Pr00k@users.noreply.github.com>
2025-10-04 13:11:40 +0300

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,8 +1,6 @@
-
-// Firebase configuration for Prok project
-// Do NOT expose this publicly unless your rules are secure!
-
-window.firebaseConfig = {
+// assets/js/firebase-config.js
+// Firebase configuration
+const firebaseConfig = {
   apiKey: "AIzaSyDfy48kwyj8iAu20hx3uJtzcbRGcfcKKn0",
   authDomain: "prok-58f05.firebaseapp.com",
   projectId: "prok-58f05",
@@ -11,3 +9,7 @@ window.firebaseConfig = {
   appId: "1:978563434886:web:d16c70551240a05c81c407",
   measurementId: "G-PWTGTT2VJT"
 };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
