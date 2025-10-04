97135afa18a76fcf57e441aa023282b02a32ab79
Pr00k <75479500+Pr00k@users.noreply.github.com>
2025-10-04 13:08:28 +0300

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,1 +1,13 @@
 
+// Firebase configuration for Prok project
+// Do NOT expose this publicly unless your rules are secure!
+
+window.firebaseConfig = {
+  apiKey: "AIzaSyDfy48kwyj8iAu20hx3uJtzcbRGcfcKKn0",
+  authDomain: "prok-58f05.firebaseapp.com",
+  projectId: "prok-58f05",
+  storageBucket: "prok-58f05.firebasestorage.app",
+  messagingSenderId: "978563434886",
+  appId: "1:978563434886:web:d16c70551240a05c81c407",
+  measurementId: "G-PWTGTT2VJT"
+};
